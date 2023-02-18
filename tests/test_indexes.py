df3050627fdc45e3a8284652f3fde6dce4973e56
Soshi Katsuta <soshi.katsuta@gmail.com>
2023-02-18 20:51:58 +0900

    Make Index.str.split generic

--- tests/test_indexes.py
+++ tests/test_indexes.py
@@ -122,7 +122,7 @@ def test_difference_none() -> None:
 def test_str_split() -> None:
     # GH 194
     ind = pd.Index(["a-b", "c-d"])
-    check(assert_type(ind.str.split("-"), pd.Index), pd.Index)
+    check(assert_type(ind.str.split("-"), "pd.Index[list[str]]"), pd.Index, list)
     check(assert_type(ind.str.split("-", expand=True), pd.MultiIndex), pd.MultiIndex)
 
 
